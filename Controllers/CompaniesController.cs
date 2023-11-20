ba681228364113d1f6a50fbaeb4b29ae31a4d586
Alyona Berezorutska <av.golberg@gmail.com>
2023-11-20 21:07:09 +0200

    [PWP-31] Connect MongoDB to BE

---
+++ Controllers/CompaniesController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PeopleManagmentSystem_API.Models;
+using PeopleManagmentSystem_API.Services;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PeopleManagmentSystem_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ICompanyService companyService;
+
+        public CompaniesController(ICompanyService companyService)
+        {
+            this.companyService = companyService;
+        }
+        // GET: api/<CompanysController>
+        [HttpGet]
+        public ActionResult<List<Company>> Get()
+        {
+            return companyService.Get();
+        }
+
+        // GET api/<CompanysController>/5
+        [HttpGet("{id}")]
+        public ActionResult<Company> Get(string id)
+        {
+            var company = companyService.Get(id);
+
+            if (company == null)
+            {
+                return NotFound($"Company with Id = {id} not found");
+            }
+
+            return company;
+        }
+
+        // POST api/<CompanysController>
+        [HttpPost]
+        public ActionResult<Company> Post([FromBody] Company company)
+        {
+            companyService.Create(company);
+
+            return CreatedAtAction(nameof(Get), new { id = company.Id }, company);
+        }
+
+        // PUT api/<CompanysController>/5
+        [HttpPut("{id}")]
+        public ActionResult Put(string id, [FromBody] Company company)
+        {
+            var existingCompany = companyService.Get(id);
+
+            if (existingCompany == null)
+            {
+                return NotFound($"Company with Id = {id} not found");
+            }
+
+            companyService.Update(id, company);
+
+            return NoContent();
+        }
+
+        // DELETE api/<CompanysController>/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(string id)
+        {
+            var company = companyService.Get(id);
+
+            if (company == null)
+            {
+                return NotFound($"Company with Id = {id} not found");
+            }
+
+            companyService.Remove(company.Id);
+
+            return Ok($"Company with Id = {id} deleted");
+        }
+    }
+}
---
+++ Models/PeopleManagmentDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace PeopleManagmentSystem_API.Models
+{
+    public class PeopleManagmentDatabaseSettings : IPeopleManagmentDatabaseSettings
+    {
+        public string CompanyCollectionName { get; set; } = String.Empty;
+        public string ConnectionString { get; set; } = String.Empty;
+        public string DatabaseName { get; set; } = String.Empty;
+    }
+}
