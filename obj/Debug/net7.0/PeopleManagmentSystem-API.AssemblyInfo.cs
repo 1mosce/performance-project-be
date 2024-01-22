35c3bba8a5952c2611743f2a4e0697c32950224f
Alyona Berezorutska <av.golberg@gmail.com>
2024-01-22 19:44:28 +0200

    [PWP-33][BE] Register Logic Update

--- obj/Debug/net7.0/PeopleManagmentSystem-API.AssemblyInfo.cs
+++ obj/Debug/net7.0/PeopleManagmentSystem-API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeopleManagmentSystem-API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba681228364113d1f6a50fbaeb4b29ae31a4d586")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeopleManagmentSystem-API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeopleManagmentSystem-API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
